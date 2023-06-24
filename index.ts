d3f7ed913a8f6360fd2bd5f7eedc5b74a9731a9d
Asliddin Otabekov <126315706+asliddinotabekov@users.noreply.github.com>
2023-06-24 19:35:40 +0500

    Enum Tyuple Union

--- index.ts
+++ index.ts
@@ -4,5 +4,34 @@ title = "adasdsa"
 let anyData : any = "wwww"
 anyData= Boolean
 let anyDataAr: [] =[]
-anyDataAr.push(["sadaads",1111  ])
-console.log(anyDataAr)
+anyDataAr = []
+
+// union
+
+let union : string | number  = "Asliddin"
+// union = true
+
+let arUnion : [string | number]= [11]
+arUnion = ["Webb"]
+
+let arUnionAny : any[string | number]= [false] // ichidagilarni atkaz qilib yuboradi 
+
+// Tyuple
+let tuple :[string,number][] ;
+
+tuple =[["sdsds",1212],["sdsds",1212]]
+
+// Enum
+
+enum dir {
+    up=2,
+    down
+}
+console.log(dir.up,dir.down)
+// drop down uchun ishlatiladi 
+
+let id : any  =123
+let uId  = <string>id
+let uIds  = id as number
+uId = '111'
+uIds = 1111
