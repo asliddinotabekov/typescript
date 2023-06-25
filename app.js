18df19fbb5c5b7c274fe3549e91234ec0f262d78
Asliddin Otabekov <126315706+asliddinotabekov@users.noreply.github.com>
2023-06-25 17:53:32 +0500

    Generics  as a function Callback function Limitations

--- app.js
+++
@@ -1,3 +0,0 @@
-let users ={name: "well",}
-
-console.log(users.name,users.age)
--- index.ts
+++ index.ts
@@ -1,37 +1,87 @@
-let usr  = {name:"webbrain" , age:111 }
-let title : string = "Webbrain"
-title = "adasdsa"
-let anyData : any = "wwww"
-anyData= Boolean
-let anyDataAr: [] =[]
-anyDataAr = []
 
-// union
+// function getData (){}
+// console.log(getData()) //undefined return qilmadi 
 
-let union : string | number  = "Asliddin"
-// union = true
+// getData()
 
-let arUnion : [string | number]= [11]
-arUnion = ["Webb"]
+function SetData (a:number,b:number): number | void {
 
-let arUnionAny : any[string | number]= [false] // ichidagilarni atkaz qilib yuboradi 
-
-// Tyuple
-let tuple :[string,number][] ;
-
-tuple =[["sdsds",1212],["sdsds",1212]]
-
-// Enum
-
-enum dir {
-    up=2,
-    down
+    // return a ** b
 }
-console.log(dir.up,dir.down)
-// drop down uchun ishlatiladi 
+// funcsiyada boshqa bir funcsiya kelishiga callback func deyiladi 
 
-let id : any  =123
-let uId  = <string>id
-let uIds  = id as number
-uId = '111'
-uIds = 1111
+// console.log(SetData (25,2))
+
+
+/// =========== Call Back  = =================
+
+
+// type param = {
+    //     key: string,
+    //     value?: number // bermasa ham boladi 
+    // }
+    
+    // const callback = (prop:param):CallProp =>{
+        //     return (a:number) => a
+        // }
+        // console.log(callback({key:"asas",value:1212}))
+
+
+        
+// type CallProp = {
+//     new () : Object
+// }
+
+
+// const getData = (fn:CallProp) : string=>{
+//     return new fn().toString()
+// }
+
+// function ConstFun(){
+//     return  Date
+// }
+
+// console.log(getData(ConstFun()))
+
+//  let ar : string[] = []
+//   function Generic(value : string ,ar:string[]) {
+//     ar.push(value)
+//     console.log(ar)
+
+//   }
+
+//   Generic("frontend",ar)
+
+// let ar : string[] = []
+
+
+
+// function Generic<Type,OutSet>( ar:Type[]) {
+//   return (value : Type) : OutSet | string=> {
+//       ar.push(value)
+//       console.log(ar)
+//       return 'sd'
+// }
+// }
+
+// const stArra =Generic<string,number>([])
+// const numArra =Generic<number,number>([])
+// stArra("Frontend")
+// numArra(1111)
+
+// function getData<Type extends {length: number}>(a:Type){
+    
+//     console.log(a.length)
+// }
+
+// getData<string>("asliddin")
+// getData<number[]>([1,2,3,4])
+// getData<number>(1)
+
+function getData (a:string):string;
+function getData (a:string , b:string, c:string):string;
+function getData (a?:string , b?:string, c?:string){
+    return "str"
+}
+
+console.log(getData("dasad","sada","scsc"))
